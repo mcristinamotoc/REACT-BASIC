0d9b31ba406a684cec1ae8cf799347c85864bdb8
Cristina M <mcristinamotoc@gmail.com>
2021-01-15 16:26:52 +0100

    Ex.3 => Nivel II

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import Book from "./book";
 export default () => {
   return (
     <div>
-      <h1>Viatge a la lluna</h1>
+      {/* <h1>Viatge a la lluna</h1> */}
+      <Book />
     </div>
   );
 };
